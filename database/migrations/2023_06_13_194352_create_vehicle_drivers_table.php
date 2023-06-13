fec50c2aa227c0f31614d2052cf0f64ef33220ec
Bilal Anwar <b.anwar21.a@gmail.com>
2023-06-14 01:02:31 +0500

    vehicle driver migration

---
+++ database/migrations/2023_06_13_194352_create_vehicle_drivers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehicleDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_drivers', function (Blueprint $table) {
+            $table->id();
+            $table->string('vehicle_id');
+            $table->string('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_drivers');
+    }
+}
