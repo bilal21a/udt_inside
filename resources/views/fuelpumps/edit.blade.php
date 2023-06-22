adcd1a07f86619f2fba4da67399664ff18282ff4
Bilal Anwar <b.anwar21.a@gmail.com>
2023-06-23 00:15:03 +0500

    changes fuel

---
+++ resources/views/fuelpumps/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Title and Top Buttons Start -->
+
+    <div class="page-title-container">
+        <div class="row">
+            <div class="col-12 col-sm-6">
+                <h1 class="mb-0 pb-0 display-4" id="title">Edit Driver</h1>
+            </div>
+        </div>
+    </div>
+
+    @include('common.alert.alert')
+    <div class="card mb-5">
+        <div class="card-body">
+            <form action="{{ route('drivers.update',$user->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                @include('drivers.forms.common_form')
+                <button type="submit" class="btn btn-primary" id="success_message">Sumbit</button>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@section('js_after')
+    <script></script>
+@endsection
---
+++ resources/views/fuelpumps/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+<style>
+    .picheight {
+        height: 96px !important;
+        width: 90px !important;
+        border-radius: 10px 0px 0px 10px;
+    }
+</style>
+@section('content')
+    @include('common.alert.alert')
+    <div class="container">
+        <!-- Title and Top Buttons Start -->
+        <div class="page-title-container">
+            <div class="row">
+                <div class="col-12 col-sm-6">
+                    <h1 class="mb-0 pb-0 display-4" id="title">Service Provider Managment</h1>
+                </div>
+            </div>
+        </div>
+
+        <a class="btn btn-icon btn-icon-start btn-primary mb-4" href="{{ route('serviceprovider.create') }}">
+            <i data-acorn-icon="plus"></i>
+            <span>Add Service Provider</span>
+        </a>
+        
+        {{-- -----Table----- --}}
+        @php
+            $tableName = 'datatable';
+            $tableData = ['Image', 'Name', 'Email', 'Phone', 'CNIC', 'Address', 'Gender', 'Actions'];
+        @endphp
+        @include('common.table.table')
+    </div>
+
+    @include('common.modal.add_edit_modal')
+@endsection
+
+@section('js_after')
+    {{-- **Show Data** --}}
+    <script>
+        var tabelDataArray = ['profile_image', 'full_name', 'email', 'phone', 'cnic', 'address', 'gender', 'action'];
+        var get_data_url = "{{ route('get_service_provider') }}"
+    </script>
+    @include('common.js.get_data')
+
+
+    {{-- **Delete Data** --}}
+    <script>
+        var delete_data_url = '{{ route('customers.destroy', ':id') }}'
+    </script>
+    @include('common.js.delete_data')
+@endsection
