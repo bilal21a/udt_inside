34ba179f4e0c1ddbfb72d5a36e42a2868953791c
Bilal Mac <b.anwar21.a@gmail.com>
2023-07-23 16:04:25 +0500

    add customer

--- resources/views/customers/add.blade.php
+++ resources/views/customers/add.blade.php
@@ -12,13 +12,24 @@
     </div>
 
     @include('common.alert.alert')
-    <div class="card mb-5">
-        <div class="card-body">
-            <form action="{{ route('customers.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @include('customers.forms.common_form')
-                <button type="submit" class="btn btn-primary" id="success_message">Sumbit</button>
-            </form>
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card custom-card">
+                <div class="card-header justify-content-between">
+                    <div class="card-title">
+                        Add Customer
+                    </div>
+                </div>
+                <div class="card-body">
+                        <form action="{{ route('customers.store') }}" method="POST" enctype="multipart/form-data" class="row gy-4">
+                            @csrf
+                            @include('customers.forms.common_form')
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary" id="success_message">Sumbit</button>
+                            </div>
+                        </form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
