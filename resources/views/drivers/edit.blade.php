44b4349556b644f4c99950a4500ba14d5fbd2c34
Bilal Anwar <b.anwar21.a@gmail.com>
2023-07-24 21:02:34 +0500

    drivers

--- resources/views/drivers/edit.blade.php
+++ resources/views/drivers/edit.blade.php
@@ -1,27 +1,31 @@
 @extends('layouts.app')
-
+@section('header')
+    Edit Driver
+@endsection
 @section('content')
-    <!-- Title and Top Buttons Start -->
-
-    <div class="page-title-container">
-        <div class="row">
-            <div class="col-12 col-sm-6">
-                <h1 class="mb-0 pb-0 display-4" id="title">Edit Driver</h1>
+    @include('common.alert.alert')
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card custom-card">
+                <div class="card-header justify-content-between">
+                    <div class="card-title">
+                        Edit Driver
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('drivers.update', $user->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        @include('drivers.forms.common_form')
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary" id="success_message">Sumbit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
 
-    @include('common.alert.alert')
-    <div class="card mb-5">
-        <div class="card-body">
-            <form action="{{ route('drivers.update',$user->id) }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                @include('drivers.forms.common_form')
-                <button type="submit" class="btn btn-primary" id="success_message">Sumbit</button>
-            </form>
-        </div>
-    </div>
 @endsection
 
 @section('js_after')
