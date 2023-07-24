d6d21b7e5b69e0a6d2bd6c6259acd3b6c7a277ed
Muhammad Yawar Tariq <yawar@solutionsurface.com>
2023-07-24 15:06:16 +0500

    edit form

--- resources/views/toll_gate/edit.blade.php
+++ resources/views/toll_gate/edit.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
-
+@section('header')
+    Edit Toll Gate
+@endsection
 @section('content')
-    <!-- Title and Top Buttons Start -->
-
-    <div class="page-title-container">
-        <div class="row">
-            <div class="col-12 col-sm-6">
-                <h1 class="mb-0 pb-0 display-4" id="title">Edit TollGate</h1>
-            </div>
-        </div>
-    </div>
-
     @include('common.alert.alert')
-    <div class="card mb-5">
-        <div class="card-body">
-            <form action="{{ route('toll_gate.update',$id) }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                @include('toll_gate.forms.common_form')
-                <button type="submit" class="btn btn-primary" id="success_message">Sumbit</button>
-            </form>
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card custom-card">
+                <div class="card-header justify-content-between">
+                    <div class="card-title">
+                        Edit Toll Gate
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('toll_gate.update', $id) }}" method="POST"
+                        enctype="multipart/form-data" class="row gy-4">
+                        @csrf
+                        @method('PUT')
+                        @include('toll_gate.forms.common_form')
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary" id="success_message">Sumbit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
@@ -27,3 +31,4 @@
 @section('js_after')
     <script></script>
 @endsection
+
